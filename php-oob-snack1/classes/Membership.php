2b6b19257d54919ad6f7a721b0c5cd9ca6849bab
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-03 12:31:50 +0200

    added .php to the path

--- php-oob-snack1/classes/Membership.php
+++ php-oob-snack1/classes/Membership.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . "classes/User";
+require_once __DIR__ . "classes/User.php";
+require_once __DIR__ . "classes/PremiumUser.php";
 
 class Membership extends PremiumUser
 {
--- php-oob-snack1/classes/PremiumUser.php
+++ php-oob-snack1/classes/PremiumUser.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . "classes/User";
+require_once __DIR__ . "classes/User.php";
 
 class PremiumUser extends User
 {
