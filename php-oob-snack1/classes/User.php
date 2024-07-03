9c9d45a4a4e7cf09b28b0c539266c15afe9be47d
MarcoMpensabene <marcompensabene@gmail.com>
2024-07-03 12:09:17 +0200

    added content to User class

---
+++ php-oob-snack1/classes/User.php
@@ -0,0 +1,30 @@
+<?php
+
+class User
+{
+    // ? Dichiarazione di visibilità . Variabili di istanza
+    private $userId;
+    private $userPassword;
+    
+    public function __construct($userId , $userPassword)
+    {
+        $this->userId = $userId;
+        $this->userPassword = $userPassword;
+    }
+
+    public function getId()
+    {
+        return $this->userId;
+    }
+
+    public function setId($id)
+    {
+        return $this->userId = $id ;
+    }
+
+    public function getPassword()
+    {
+        return $this->userPassword;
+    }
+
+}
